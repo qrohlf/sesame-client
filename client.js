27ea592333bc171a98d36f62f7ef6e292d8e2ddf
Quinn Rohlf <qr@qrohlf.com>
2014-10-06 12:54:11 -0700

    mock-up some behavior for testing

--- client.js
+++ client.js
@@ -1,14 +1,53 @@
-var socket = require('socket.io-client')('http://localhost:3000');
+var socket = require('socket.io-client')(process.env.SERVER_PATH || 'http://localhost:3000');
+
 socket.on('connect', function(){
-
   console.log('connected!');
-
-  socket.on('event', function(data){
-    console.log(data);
-  });
-
-  socket.on('disconnect', function(){
-    console.log('disconnected!');
-  });
-
 });
+
+socket.on('door-command', function(cmd){
+
+  console.log('door-command '+cmd+' recieved')
+  switch(cmd) {
+    case 'open':
+      open_door();
+      break;
+    case 'close':
+      close_door();
+      break;
+    case 'toggle':
+      toggle_door();
+      break;
+  }
+});
+
+socket.on('disconnect', function(){
+  console.log('disconnected!');
+});
+
+socket.on('reconnect', function(){
+  console.log('reconnected!');
+});
+
+socket.on('reconnecting', function(num) {
+  console.log('attempting to reconnect');
+  console.log('attempt number ' + num);
+});
+
+function open_door() {
+  socket.emit('door-status', 'opening')
+  setTimeout(function() {
+    socket.emit('door-status', 'opened')
+  }, 5000);
+}
+
+
+function close_door() {
+  socket.emit('door-status', 'closing')
+  setTimeout(function() {
+    socket.emit('door-status', 'closed')
+  }, 5000);
+}
+
+function toggle_door() {
+  (Math.random() > .5) ? open_door() : close_door();
+}
